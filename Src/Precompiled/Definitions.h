6a1fe15ec761fc0dbcd7748e9f5b1a47b9a7281e
Hazno21688 <62346467+Hazno-dev@users.noreply.github.com>
2026-02-02 06:07:24 +0000

    new macros!!

--- Src/Precompiled/Definitions.h
+++ Src/Precompiled/Definitions.h
@@ -1,11 +1,18 @@
 // Hazno - 2026
 
+// ReSharper disable CppInconsistentNaming
 #pragma once
 
 #if defined(ATLAS_PLATFORM_MSVC)
-  #define ATLAS_FORCEINLINE __forceinline
+#define ATLAS_FORCE_INLINE __forceinline
 #elif defined(ATLAS_PLATFORM_CLANG) || defined(ATLAS_PLATFORM_GNU)
-  #define ATLAS_FORCEINLINE inline __attribute__((always_inline))
+#define __int64 long long
+#define __int32 int
+#define __int16 short
+#define __int8  char
+#define ATLAS_FORCE_INLINE inline __attribute__((always_inline))
 #else
-  #define ATLAS_FORCEINLINE inline
+#define ATLAS_FORCE_INLINE inline
 #endif
+
+#define ATLAS_NODISCARD [[nodiscard]]
--- Src/Precompiled/Macros.h
+++ Src/Precompiled/Macros.h
@@ -25,6 +25,10 @@
  */
 #define ATLAS_PADDING(size) uint8 DTL_ATLAS_MERGE_WRAP(pad, __COUNTER__)[size]
 
+#define ATLAS_FOR_EACH(macro, ...) DTL_ATLAS_FOR_EACH(macro, __VA_ARGS__)
+
+#define ATLAS_UNPARENS(args) DTL_ATLAS_UNPARENS(args)
+
 /*
  * Define a stub function returning specified type
  *
@@ -58,16 +62,43 @@
 #define ATLAS_GET_MACRO_4(M1, M2, M3, M4, ...) DTL_ATLAS_GET_MACRO_4(__VA_ARGS__, M4, M3, M2, M1, FILLER)
 
 
-
-
 //
 //Detail macros. Not intended for direct use
 //
 
-#define DTL_ATLAS_IGNORE(x)
-#define DTL_ATLAS_ID(x) x
+//Util
+
+#define DTL_ATLAS_PARENS ()
+#define DTL_ATLAS_EMPTY(x)
+#define DTL_ATLAS_EXPAND(x) x
 #define DTL_ATLAS_STRING(x) #x
 #define DTL_ATLAS_MERGE(x, y) x ## y
 #define DTL_ATLAS_MERGE_WRAP(x, y) DTL_ATLAS_MERGE(x, y)
 
+//Macro getter impl
+
 #define DTL_ATLAS_GET_MACRO_4(M4, M3, M2, M1, TARGET, ...) TARGET
+
+//Foreach impl
+//From https://www.scs.stanford.edu/~dm/blog/va-opt.html
+
+#define DTL_ATLAS_EXPAND0(...) DTL_ATLAS_EXPAND4(DTL_ATLAS_EXPAND4(DTL_ATLAS_EXPAND4(DTL_ATLAS_EXPAND4(__VA_ARGS__))))
+#define DTL_ATLAS_EXPAND4(...) DTL_ATLAS_EXPAND3(DTL_ATLAS_EXPAND3(DTL_ATLAS_EXPAND3(DTL_ATLAS_EXPAND3(__VA_ARGS__))))
+#define DTL_ATLAS_EXPAND3(...) DTL_ATLAS_EXPAND2(DTL_ATLAS_EXPAND2(DTL_ATLAS_EXPAND2(DTL_ATLAS_EXPAND2(__VA_ARGS__))))
+#define DTL_ATLAS_EXPAND2(...) DTL_ATLAS_EXPAND1(DTL_ATLAS_EXPAND1(DTL_ATLAS_EXPAND1(DTL_ATLAS_EXPAND1(__VA_ARGS__))))
+#define DTL_ATLAS_EXPAND1(...) __VA_ARGS__
+
+#define DTL_ATLAS_FOR_EACH(macro, ...)										\
+	__VA_OPT__(DTL_ATLAS_EXPAND0(DTL_ATLAS_FOR_EACH_HELPER(macro, __VA_ARGS__)))
+
+#define DTL_ATLAS_FOR_EACH_HELPER(macro, a1, ...)								\
+	macro a1														\
+	__VA_OPT__(, DTL_ATLAS_FOR_EACH_AGAIN DTL_ATLAS_PARENS (macro, __VA_ARGS__))
+
+#define DTL_ATLAS_FOR_EACH_AGAIN() DTL_ATLAS_FOR_EACH_HELPER
+
+//Unparent impl
+//From https://stackoverflow.com/a/46311121
+
+#define DTL_ATLAS_REALLY_UNPARENS(...) ATLAS_FOR_EACH(typename , __VA_ARGS__)
+#define DTL_ATLAS_UNPARENS(args) DTL_ATLAS_EXPAND(DTL_ATLAS_REALLY_UNPARENS args)
