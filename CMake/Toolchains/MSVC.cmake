a0434db496bd04d61ea8ccf44ad620e5dbba8f81
Hazno21688 <62346467+Hazno-dev@users.noreply.github.com>
2026-02-02 06:07:52 +0000

    export pdb by default?

--- CMake/Toolchains/MSVC.cmake
+++ CMake/Toolchains/MSVC.cmake
@@ -1,6 +1,6 @@
 mark_as_advanced(CMAKE_TOOLCHAIN_FILE)
 
-add_compile_options(/Zc:preprocessor)
+add_compile_options(/Zc:preprocessor /Zi)
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
 
 message(STATUS "Current list dir: ${CMAKE_CURRENT_LIST_DIR}")
