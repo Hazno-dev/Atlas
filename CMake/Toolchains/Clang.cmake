a69b57c2a2a6dd7abd8646a0c7b8531bc9a23597
Hazno21688 <62346467+Hazno-dev@users.noreply.github.com>
2026-02-08 22:29:24 +0000

    fix libc++.dll linking error

--- CMake/Toolchains/Clang.cmake
+++ CMake/Toolchains/Clang.cmake
@@ -6,10 +6,11 @@ set(CMAKE_EXPERIMENTAL_CXX_IMPORT_STD "d0edc3af-4c50-42ea-a356-e2862fe7a444")
 set(CMAKE_CXX_MODULE_STD ON)
 
 add_compile_options(-w -ffunction-sections -fdata-sections -fbuiltin
-        -fno-stack-protector -std=c++23 -fms-extensions)
+        -fno-stack-protector -std=c++23 -fms-extensions -stdlib=libc++)
 
-add_compile_definitions(_WINDOWS _WIN32 _WINSOCKAPI_ NOGDI _USRDLL FMT_IMPORT_STD FMT_MODULE=1 FMT_ATTACH_TO_GLOBAL_MODULE
-        SPDLOG_FMT_EXTERNAL WINVER=0x0601 _WIN32_WINNT=0x0601)
+add_compile_definitions(_WINDOWS _WIN32 _WINSOCKAPI_ NOGDI _USRDLL WINVER=0x0601 _WIN32_WINNT=0x0601 ${ATLAS_DEFAULT_DEFINITIONS})
+
+add_link_options(-static-libgcc -static-libstdc++)
 
 message(STATUS "Current list dir: ${CMAKE_CURRENT_LIST_DIR}")
 include(${CMAKE_CURRENT_LIST_DIR}/Vcpkg.cmake)
